d80eaebb6ecb0f650a2339f4dd9537f5fa5038f0
Igor Nascimento <igorln96@gmail.com>
2020-12-14 18:46:47 -0300

    codeclimate fixed

--- script.js
+++ script.js
@@ -9,77 +9,80 @@ function numberGenerator() {
   b = Math.floor(Math.random() * 255);
 }
 numberGenerator();
-let rgbColorRight = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+let rgbColorRight = `rgb(${r}, ${g}, ${b})`;
 
 // Altera o RGB no titulo
-let rgbTitle = document.getElementById('rgb-color');
-rgbTitle.innerHTML = '(' + r + ', ' + g + ', ' + b + ')';
+const rgbTitle = document.getElementById('rgb-color');
+rgbTitle.innerHTML = `(${r}, ${g}, ${b})`;
+
+// Criar placar
+const score = document.getElementById('score');
+let points = 0;
+let correctAnswer = 0;
+const answer = document.getElementById('answer');
+const scoreCalculator = () => {
+  if (answer.innerHTML === 'Acertou!') {
+    if (correctAnswer === 0) {
+      points += 3;
+      correctAnswer += 1;
+      score.innerText = points;
+    }
+  } else {
+    points -= 1;
+    score.innerText = points;
+  }
+};
+
+// Acertou ou errou a cor
+function check(event) {
+  if (event.target.style.backgroundColor === rgbColorRight) {
+    answer.innerHTML = 'Acertou!';
+    scoreCalculator();
+  } else {
+    answer.innerHTML = 'Errou! Tente novamente!';
+    scoreCalculator();
+  }
+}
 
 // Cria as 6 bolas
 const ballsCase = document.getElementById('ballsCase');
 function createBalls() {
   for (let i = 0; i < 6; i += 1) {
-      let ball = document.createElement('div');
-      ball.className = "ball";
-      ball.addEventListener('click', check)
-      ballsCase.appendChild(ball);
-    }
+    const ball = document.createElement('div');
+    ball.className = 'ball';
+    ball.addEventListener('click', check);
+    ballsCase.appendChild(ball);
+  }
 }
 createBalls();
 
 // Colore as 6 bolas
-let ball = document.getElementsByClassName('ball')
+const ball = document.getElementsByClassName('ball');
 function color() {
-    let rgbColor = 'rgb(' + r + ', ' + g + ', ' + b + ')';
-    for (let i = 0; i < ball.length; i += 1) {
-        numberGenerator();
-        rgbColor = 'rgb(' + r + ', ' + g + ', ' + b + ')';
-        ball[i].style.backgroundColor = rgbColor;
-    }    
+  let rgbColor = `rgb(${r}, ${g}, ${b})`;
+  for (let i = 0; i < ball.length; i += 1) {
+    numberGenerator();
+    rgbColor = `rgb(${r}, ${g}, ${b})`;
+    ball[i].style.backgroundColor = rgbColor;
+  }
 }
 color();
 
 // Colore a bola certa
-function colorRight() {
-    let position = Math.floor(Math.random() * 5);
-    ball[position].style.backgroundColor = rgbColorRight;  
-}
+const colorRight = () => {
+  const position = Math.floor(Math.random() * 5);
+  ball[position].style.backgroundColor = rgbColorRight;
+};
 colorRight();
 
-// Acertou ou errou a cor
-let answer = document.getElementById("answer");
-function check (event) {
-    if (event.target.style.backgroundColor === rgbColorRight) {
-        answer.innerHTML = "Acertou!";
-        scoreCalculator()
-    } else {
-        answer.innerHTML = "Errou! Tente novamente!";
-        scoreCalculator()
-    }
-}
-
 // Botão de resetar
-let resetButton = document.getElementById("reset-game");
-resetButton.addEventListener("click", function () {
-    numberGenerator();
-    rgbColorRight = 'rgb(' + r + ', ' + g + ', ' + b + ')';
-    rgbTitle.innerHTML = '(' + r + ', ' + g + ', ' + b + ')';
-    color()
-    colorRight()
-    answer.innerHTML = 'Escolha uma cor'
+const resetButton = document.getElementById('reset-game');
+resetButton.addEventListener('click', function () {
+  numberGenerator();
+  rgbColorRight = `rgb(${r}, ${g}, ${b})`;
+  rgbTitle.innerHTML = `(${r}, ${g}, ${b})`;
+  color();
+  colorRight();
+  answer.innerHTML = 'Escolha uma cor"';
+  correctAnswer = 0;
 });
-
-// Criar placar
-let scoreText = document.getElementById("scoreValue");
-let score = document.getElementById("score");
-let points = 0;
-function scoreCalculator () {
-    if (answer.innerHTML == "Acertou!") {
-        points += 3;
-        score.innerText = points;
-    } else {
-        points -= 1;
-        score.innerText = points;
-    }
-    localStorage.setItem("score", score.innerText)
-}
